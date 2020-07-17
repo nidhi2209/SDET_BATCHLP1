4775c5861025dbfe362441bc74bb5c6169bb3775
nidhi2209 <nidhi.bhatnagar@in.ibm.com>
2020-07-17 10:14:53 +0530

    sample commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, I am Nidhi!");
+		int num=80;
+		if(num<=80) {
+			System.out.println("Less than 80");
+			
+		}
+		else
+			System.out.println("Not equal to 80");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Test_1.java
@@ -0,0 +1,14 @@
+public class Test_1 
+{
+    public static void main(String args[]) 
+    {
+    	   
+		   int n=10;
+       for(int i=1;i<=10;i++)
+        {
+            System.out.println("Member "+ i +" Age is "+n*i);
+        }
+       
+    }
+}
+
